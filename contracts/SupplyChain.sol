9a84a7c05ac668153e99dbf19b55c5ab697e979e
TomaMajic <tomamajic111@gmail.com>
2019-11-16 19:37:06 +0100

    Supply chain contract final version # Solidity tests added

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -8,103 +8,117 @@ pragma solidity ^0.5.0;
 
 contract SupplyChain {
 
-  /* set owner */
-  address owner;
+    address public owner;
+    uint public skuCount;
 
-  /* Add a variable called skuCount to track the most recent sku # */
+    mapping (uint => Item) public items;
 
-  /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
-     Call this mappings items
-  */
+    enum State {
+        ForSale,
+        Sold,
+        Shipped,
+        Received
+    }
 
-  /* Add a line that creates an enum called State. This should have 4 states
-    ForSale
-    Sold
-    Shipped
-    Received
-    (declaring them in this order is important for testing)
-  */
+    struct Item {
+        string name;
+        uint sku;
+        uint price;
+        State state;
+        address payable seller;
+        address payable buyer;
+    }
 
-  /* Create a struct named Item.
-    Here, add a name, sku, price, state, seller, and buyer
-    We've left you to figure out what the appropriate types are,
-    if you need help you can ask around :)
-    Be sure to add "payable" to addresses that will be handling value transfer
-  */
+    event LogForSale(uint sku);
+    event LogSold(uint sku);
+    event LogShipped(uint sku);
+    event LogReceived(uint sku);
 
-  /* Create 4 events with the same name as each possible State (see above)
-    Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
-    Each event should accept one argument, the sku */
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
-/* Create a modifer that checks if the msg.sender is the owner of the contract */
+    modifier verifyCaller(address _address) {
+        require (msg.sender == _address);
+        _;
+    }
 
-  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
+    modifier paidEnough(uint _price) {
+        require(msg.value >= _price);
+        _;
+    }
 
-  modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
-  modifier checkValue(uint _sku) {
-    //refund them after pay for item (why it is before, _ checks for logic before func)
-    _;
-    uint _price = items[_sku].price;
-    uint amountToRefund = msg.value - _price;
-    items[_sku].buyer.transfer(amountToRefund);
-  }
+    modifier checkValue(uint _sku) {
+        _;
+        uint _price = items[_sku].price;
+        uint amountToRefund = msg.value - _price;
+        items[_sku].buyer.transfer(amountToRefund);
+    }
 
-  /* For each of the following modifiers, use what you learned about modifiers
-   to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. 
-   Note that the uninitialized Item.State is 0, which is also the index of the ForSale value,
-   so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
-   Hint: What item properties will be non-zero when an Item has been added?
-   */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+    modifier forSale(uint _sku) {
+        Item memory item = items[_sku];
+        require(item.state == State.ForSale && item.seller != address(0x0));
+        _;
+    }
 
+    modifier sold(uint _sku) {
+        Item memory item = items[_sku];
+        require(item.state == State.Sold);
+        _;
+    }
 
-  constructor() public {
-    /* Here, set the owner as the person who instantiated the contract
-       and set your skuCount to 0. */
-  }
+    modifier shipped(uint _sku) {
+        Item memory item = items[_sku];
+        require(item.state == State.Shipped);
+        _;
+    }
 
-  function addItem(string memory _name, uint _price) public returns(bool){
-    emit LogForSale(skuCount);
-    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
-    skuCount = skuCount + 1;
-    return true;
-  }
+    modifier received(uint _sku) {
+        Item memory item = items[_sku];
+        require(item.state == State.Received);
+        _;
+    }
 
-  /* Add a keyword so the function can be paid. This function should transfer money
-    to the seller, set the buyer as the person who called this transaction, and set the state
-    to Sold. Be careful, this function should use 3 modifiers to check if the item is for sale,
-    if the buyer paid enough, and check the value after the function is called to make sure the buyer is
-    refunded any excess ether sent. Remember to call the event associated with this function!*/
+    constructor() public {
+        owner = msg.sender;
+        skuCount = 0;
+    }
 
-  function buyItem(uint sku)
-    public
-  {}
+    function addItem(string memory _name, uint _price) public returns(bool) {
+        emit LogForSale(skuCount);
+        items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
+        skuCount = skuCount + 1;
+        return true;
+    }
 
-  /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
-  is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
-  function shipItem(uint sku)
-    public
-  {}
+    function buyItem(uint _sku) public payable forSale(_sku) paidEnough(items[_sku].price) checkValue(_sku) {
+        Item storage item = items[_sku];
+        item.seller.transfer(item.price);
+        item.buyer = msg.sender;
+        item.state = State.Sold;
+        emit LogSold(_sku);
+    }
 
-  /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
-  is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
-  function receiveItem(uint sku)
-    public
-  {}
+    function shipItem(uint _sku) public sold(_sku) verifyCaller(items[_sku].seller) {
+        Item storage item = items[_sku];
+        item.state = State.Shipped;
+        emit LogShipped(_sku);
+    }
 
-  /* We have these functions completed so we can run tests, just ignore it :) */
-  function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
-    name = items[_sku].name;
-    sku = items[_sku].sku;
-    price = items[_sku].price;
-    state = uint(items[_sku].state);
-    seller = items[_sku].seller;
-    buyer = items[_sku].buyer;
-    return (name, sku, price, state, seller, buyer);
-  }
+    function receiveItem(uint _sku) public shipped(_sku) verifyCaller(items[_sku].buyer) {
+        Item storage item = items[_sku];
+        item.state = State.Received;
+        emit LogReceived(_sku);
+    }
 
+    function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
+        name = items[_sku].name;
+        sku = items[_sku].sku;
+        price = items[_sku].price;
+        state = uint(items[_sku].state);
+        seller = items[_sku].seller;
+        buyer = items[_sku].buyer;
+        return (name, sku, price, state, seller, buyer);
+    }
 }
--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -4,24 +4,163 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
-contract TestSupplyChain {
+contract Participant {
+    SupplyChain private supplyChain;
 
-    // Test for failing conditions in this contracts:
-    // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+    function () external payable {}
+
+    constructor(SupplyChain _supplyChain) public {
+        supplyChain = _supplyChain;
+    }
+
+    function addItem(string memory _name, uint _price) public {
+        supplyChain.addItem(_name, _price);
+    }
+
+    function buy(uint _sku, uint _amount) public returns (bool success) {
+        (success, ) = address(supplyChain).call.gas(1000000).value(_amount)(abi.encodeWithSignature("buyItem(uint256)", _sku));
+    }
+
+    function ship(uint _sku) public returns (bool success) {
+        (success, ) = address(supplyChain).call(abi.encodeWithSignature("shipItem(uint256)", _sku));
+    }
+
+    function receive(uint _sku) public returns (bool success) {
+        (success, ) = address(supplyChain).call(abi.encodeWithSignature("receiveItem(uint256)", _sku));
+    }
+}
+
+contract SupplyChainCreator {
+    SupplyChain private supplyChain;
+
+    function () external payable {}
+
+    function set(SupplyChain _supplyChain) public {
+        supplyChain = _supplyChain;
+    }
+}
+
+contract TestSupplyChain {
+    uint public initialBalance = 1 ether;
+    uint public participantInitialBalance = 5000 wei;
+    uint public itemPrice = 1000 wei;
+    uint public excessAmount = 2000 wei;
+
+    Participant private seller;
+    Participant private buyer;
+
+    SupplyChain private supplyChain;
+    SupplyChainCreator private supplyChainCreator;
+
+    function () external payable {}
+
+    constructor() public payable {}
+
+    function setupContracts() public {
+        supplyChainCreator = new SupplyChainCreator();
+        supplyChain = new SupplyChain();
+        supplyChainCreator.set(supplyChain);
+
+        seller = createParticipant(supplyChain);
+        buyer = createParticipant(supplyChain);
+
+        seller.addItem("TestItem", itemPrice);
+    }
+
+    function createParticipant(SupplyChain _supplyChain) internal returns (Participant) {
+        Participant participant = new Participant(_supplyChain);
+        address(participant).transfer(participantInitialBalance);
+        return participant;
+    }
+
+    modifier setup {
+        setupContracts();
+        _;
+    }
+
+    modifier buyBefore(uint _sku) {
+        bool status = buyer.buy(_sku, excessAmount);
+        Assert.equal(status, true, "Purchase should be successful");
+        _;
+    }
+
+    modifier shipBefore(uint _sku) {
+        bool status = seller.ship(_sku);
+        Assert.equal(status, true, "The item should ship successfully");
+        _;
+    }
+
+    modifier receiveBefore(uint _sku) {
+        bool status = buyer.receive(0);
+        Assert.equal(status, true, "The item should be received");
+        _;
+    }
 
     // buyItem
-
     // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+    function testPurchaseWithNoSufficientFunds() public setup {
+        bool status = buyer.buy(0, 1 wei);
+        Assert.isFalse(status, "Purchase should fail if user does not send enough funds");
+    }
+
+    // // test for purchasing an item that is not for Sale
+    function testBuyingANonExistantItem() public setup {
+        bool status = buyer.buy(1, excessAmount);
+        Assert.isFalse(status, "Purchase should fail if the item does not exist");
+    }
+
+    function testBuyingAnItemNotForSale() public setup buyBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 1, "The state should change to Sold");
+        bool status = seller.buy(0, excessAmount);
+        Assert.isFalse(status, "Purchasing an already sold item should fail");
+    }
 
     // shipItem
-
     // test for calls that are made by not the seller
+    function testShippingIfNotTheSeller() public setup buyBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 1, "The state of the item should be Sold");
+        bool status = buyer.ship(0);
+        Assert.isFalse(status, "Shipping can only be made by the seller");
+    }
+
     // test for trying to ship an item that is not marked Sold
+    function testShippingIfItemNotSold() public setup {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 0, "The state of the item should be ForSale");
+        bool status = seller.ship(0);
+        Assert.isFalse(status, "Shipping can only be made if the item has been sold");
+    }
+
+    function testShippingIfItemAlreadyShipped() public setup buyBefore(0) shipBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 2, "The state of the item should be Shipped");
+        bool status = seller.ship(0);
+        Assert.isFalse(status, "The shipmet should fail since the item is already shipped");
+    }
 
     // receiveItem
-
     // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
+    function testReceivingAnItemIfNotTheBuyer() public setup buyBefore(0) shipBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 2, "The state of the item should be Shipped");
+        bool status = seller.receive(0);
+        Assert.isFalse(status, "Only the buyer should be able to receive the item");
+    }
 
+    // test calling the function on an item not marked Shipped
+    function testReceivingAnItemThatWasNotShipped() public setup buyBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 1, "The state of the item should be Sold");
+        bool status = buyer.receive(0);
+        Assert.isFalse(status, "Cannot receive the item that has not been shipped");
+    }
+
+    function testReceivingAnAlreadyReceivedItem() public setup buyBefore(0) shipBefore(0) receiveBefore(0) {
+        (,,,SupplyChain.State state,,) = supplyChain.items(0);
+        Assert.equal(uint(state), 3, "The state of the item should be Received");
+        bool status = buyer.receive(0);
+        Assert.isFalse(status, "Item already received");
+    }
 }
